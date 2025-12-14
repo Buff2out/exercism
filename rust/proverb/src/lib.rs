d5f25bd8bfe45ef7eea8c283cdc43bd6e2b8c56b
Rorik Star Platinum <kv-rust-dev@proton.me>
2025-12-15 00:04:32 +0300

    proverb

---
+++ rust/proverb/src/lib.rs
@@ -0,0 +1,14 @@
+pub fn build_proverb(list: &[&str]) -> String {
+    if list.is_empty() {
+        return String::new();
+    }
+
+    list.windows(2)
+        .map(|pair| format!("For want of a {} the {} was lost.", pair[0], pair[1]))
+        .chain(std::iter::once(format!(
+            "And all for the want of a {}.",
+            list[0]
+        )))
+        .collect::<Vec<_>>()
+        .join("\n")
+}
