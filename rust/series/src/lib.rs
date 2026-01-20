4019513af975412c4b79adea5d7ef9a072a9d1ce
rorikstr <slava.070299@gmail.com>
2026-01-20 20:44:06 +0300

    _

--- rust/series/src/lib.rs
+++ rust/series/src/lib.rs
@@ -2,5 +2,10 @@ pub fn series(digits: &str, len: usize) -> Vec<String> {
     if len > digits.len() {
         return Vec::new();
     }
-    Vec::new();
+    digits
+        .chars()
+        .collect::<Vec<char>>()
+        .windows(len)
+        .map(|win| win.iter().collect())
+        .collect()
 }
