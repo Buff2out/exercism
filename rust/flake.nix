2c3933e8105b318e9ea1a53c3eaaab91af168176
rorikstr <slava.070299@gmail.com>
2026-02-04 16:32:10 +0300

    flake nix to rust

--- rust/flake.nix
+++ rust/flake.nix
@@ -1,24 +1,32 @@
 {
-  description = "async-rust";
+  description = "trails-server";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
+
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system};
       in {
         devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            openssl
+          nativeBuildInputs = with pkgs; [ 
+            pkg-config 
+            cargo 
+            rustc
+            rust-analyzer
+            hurl
           ];
+
+          buildInputs = with pkgs; [ 
+            openssl 
+          ];
+
           shellHook = ''
             export OPENSSL_DIR="${pkgs.openssl.dev}"
             export OPENSSL_LIB_DIR="${pkgs.openssl.out}/lib"
-            export PKG_CONFIG_PATH="${pkgs.openssl.dev}/lib/pkgconfig"
           '';
         };
       }
     );
 }
-
