82d03f7da54998ce2d65f3328b8a171f93218118
Vyacheslav Karakin <kv-rust-dev@proton.me>
2026-02-07 19:45:29 +0300

    lack of tests

--- rust/eliuds-eggs/src/lib.rs
+++ rust/eliuds-eggs/src/lib.rs
@@ -1,3 +1,13 @@
 pub fn egg_count(display_value: u32) -> usize {
-    todo!("count the eggs in {display_value}")
+    let mut display_value_mut = display_value;
+    let mut result = 0;
+    while display_value_mut != 0 {
+        if 1 == display_value_mut % 2 {
+            result += 1;
+            display_value_mut -= 1;
+        }
+        display_value_mut /= 2;
+    }
+
+    result
 }
