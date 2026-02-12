66e0940830958db44d0e1a1b7914341bef9c04ee
Vyacheslav Karakin <kv-rust-dev@proton.me>
2026-02-12 21:02:41 +0300

    _

--- rust/acronym/src/lib.rs
+++ rust/acronym/src/lib.rs
@@ -1,3 +1,16 @@
 pub fn abbreviate(phrase: &str) -> String {
-    todo!("Given the phrase '{phrase}', return its acronym");
+    std::iter::once(' ')
+        .chain(phrase.chars())
+        .zip(phrase.chars())
+        .filter_map(|(prev, curr)| {
+            let is_word_started = curr.is_alphabetic()
+                && ((!prev.is_alphabetic() && prev != '\'')
+                    || (prev.is_lowercase() && curr.is_uppercase()));
+            if is_word_started {
+                Some(curr.to_ascii_uppercase())
+            } else {
+                None
+            }
+        })
+        .collect()
 }
